63cb96c732fc2f23b332168da464565c8ffd2638
adrian <adrian.reyes@zahonero.com>
2023-05-17 16:31:54 +0200

    CRUD users backend

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\ControllerUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,23 +23,23 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    Route::apiResource('/users', UserController::class);
+    Route::apiResource('/users', ControllerUser::class);
 });
 
-//Obtenemos las pistas.
+//Obtiene las pistas.
 Route::get('/pistas', [\App\Http\Controllers\Api\ControllerPistas::class,'index']);
 
-//Mostrar una pista
+//Muestra una pista.
 Route::get('/pistas/{pista}', [\App\Http\Controllers\Api\ControllerPistas::class, 'show']);
 
-//Creamos una pista.
+//Crea una pista.
 Route::post('/pistas', [\App\Http\Controllers\Api\ControllerPistas::class,'store'])->name('guardarPista');
 
-//Modificamos una pista.
+//Modifica una pista.
 Route::put('/pistas/{pista}', [\App\Http\Controllers\Api\ControllerPistas::class, 'update']);
 Route::patch('/pistas/{pista}', [\App\Http\Controllers\Api\ControllerPistas::class, 'update']);
 
-//Eliminamos una pista.
+//Elimina una pista.
 Route::delete('/pistas/{pista}', [\App\Http\Controllers\Api\ControllerPistas::class, 'destroy']);
 
 
